1434e96643544577bb77791e0a796d352c3a4134
Diogo Gomes <dgomes@av.it.pt>
2017-12-06 00:44:56 +0000

    Initial working version
    It works!

--- twitterimages/__main__.py
+++ twitterimages/__main__.py
@@ -1,34 +1,66 @@
+import magic
 import logging
 import sys
 import argparse
 import os
 from bot import Bot 
-from images import DB
+from db import DB,Status
 from config import p as Config
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(name)s %(message)s', level=logging.DEBUG)
+
+def post(bot, db):
+    img = db.get_image()
+    logging.info("Posting: {}".format(img))
+    if len(img.statuses):
+        new_caption = (0,img.caption)
+        for s in img.statuses:
+            favs = bot.get_favs(s.twitter_status_id)
+            if favs > new_caption[0]:
+                new_caption = (favs, s.twitter_status_text)
+        img.caption = new_caption[1].replace(Config.get("twitter", "screen_name"), '')
+
+    if img.caption != None: 
+        status = img.caption
+    else:
+        status = Config.get("messages", "missing_caption")
+    status_id = bot.publish(os.path.join(Config.get("main", "images_dir"),img.filename), status)
+    img.times_published+=1
+    img.statuses.append(Status(twitter_status_id=status_id, twitter_status_text=status))
+    logging.info("Updating image to {}".format(img))
+    db.update(img)
+
+def sync(db):
+    db.sync_dir(Config.get("main", "images_dir"))
+
+def process_replies(bot, db):
+    replies = bot.replies(min_fav=1)
+    logging.debug("Replies: {}".format(replies))
+    for image_status_id,replies in replies.items():
+        img = db.get_image(image_status_id)
+        logging.info("Replies for {}".format(img))
+        for reply in replies:
+            logging.info("Reply: {}".format(reply))
+            if not reply.twitter_status_id in [s.twitter_status_id for s in img.statuses]:
+                img.statuses.append(reply)
+                db.update(img)
 
 def main(args):
     logging.info("TwitterImage v0") 
-    images_dir = Config.get("main", "images_dir") 
  
+    b = Bot()
     db = DB(Config.get("main","db_url"))
     if args.sync:
-        db.sync_dir(images_dir)
-    img = db.get_image()
-    logging.info(img)
-    b = Bot()
-    if img.status != None: 
-        status = img.status
-    else:
-        status = Config.get("messages", "missing_status")
-    b.publish(os.path.join(images_dir,img.filename), status)
-    db.update_image(img)
+        sync(db)
+    process_replies(b, db)
+    if args.post:
+        post(b, db)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Twitter Image Bot")
-    parser.add_argument('--sync', help='syncronize imagedir with database', default=False)
+    parser.add_argument('--sync', help='syncronize imagedir with database', action='store_true')
+    parser.add_argument('--post', help='post an image', action='store_true')
 
     args = parser.parse_args()
     sys.exit(main(args))
--- twitterimages/bot.py
+++ twitterimages/bot.py
@@ -1,6 +1,11 @@
 import logging
+logging.getLogger('tweepy').setLevel(logging.WARN)
+logging.getLogger('urllib3').setLevel(logging.WARN)
+logging.getLogger('requests_oauthlib').setLevel(logging.WARN)
+logging.getLogger('oauthlib').setLevel(logging.WARN)
 import tweepy
 from config import p as Config
+from db import Status 
 
 class Bot:
     def __init__(self):
@@ -19,5 +24,27 @@ class Bot:
 
     def publish(self, filename, status=None, in_reply_to_status_id=None):
         status = self.api.update_with_media(filename, status, in_reply_to_status_id)
-        logging.debug(status)
+        logging.debug("Published new status: {}".format(status.id_str))
+        return status.id_str
 
+    def get_favs(self, status_id):
+        s = self.api.get_status(status_id)
+        return s.favorite_count
+
+    def replies(self, min_fav=1):
+        user_timeline = self.api.user_timeline()
+        if len(user_timeline) == 0:
+            return dict()
+        last = min([status.id_str for status in user_timeline])
+        me = self.api.me()
+
+        replies = dict()
+
+        for t in self.api.search(q="@{}".format(me.screen_name), since_id=last):
+            if t.favorite_count >= min_fav:
+                if not t.in_reply_to_status_id_str in replies:
+                    replies[t.in_reply_to_status_id_str] = []
+
+                replies[t.in_reply_to_status_id_str].append(Status(twitter_status_id=t.id_str, twitter_status_text=t.text))
+      
+        return replies
