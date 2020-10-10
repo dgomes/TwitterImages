f08eb7f3f471d6ed01a8c76a4732558814158e80
Diogo Gomes <diogogomes@gmail.com>
2020-10-10 13:55:37 +0100

    don't follow

--- twitterimages/__main__.py
+++ twitterimages/__main__.py
@@ -14,14 +14,17 @@ def post(bot, db):
     try: 
         img = db.get_image()
         logging.info("Posting: {}".format(img))
-        if len(img.statuses):
+        if False: #len(img.statuses):
             new_caption = (0,img.caption)
             for s in img.statuses:
-                favs = bot.get_favs(s.twitter_status_id)
-                if favs > new_caption[0]:
-                    new_caption = (favs, s.twitter_status_text)
+                try:
+                    favs = bot.get_favs(s.twitter_status_id)
+                    if favs > new_caption[0]:
+                        new_caption = (favs, s.twitter_status_text)
+                except Exception as e:
+                    logging.error("%s ignoring", e)
             img.caption = new_caption[1].replace(Config.get("twitter", "screen_name"), '')
-
+        
         if img.caption != None: 
             status = img.caption
         else:
@@ -32,7 +35,7 @@ def post(bot, db):
         logging.info("Updating image to {}".format(img))
         db.update(img)
     except Exception as e:
-        logging.error(e)
+        logging.error("Error while posting: %s", e)
 
 def sync(db):
     db.sync_dir(Config.get("main", "images_dir"))
@@ -74,6 +77,10 @@ def main(args):
                 logging.info("Following {}".format(u.screen_name))
                 b.follow(u.id_str)
 
+    if args.follow:
+        u = b.follow(args.follow)
+        logging.info("Following {}".format(u.screen_name))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Twitter Image Bot")
@@ -82,6 +89,7 @@ if __name__ == '__main__':
     parser.add_argument('--post', help='post an image', action='store_true')
     parser.add_argument('--follow_search', help='follow users talking about', action='store')
     parser.add_argument('--follow_location', help='follow users with provided location', action='store')
+    parser.add_argument('--follow', help='follow user', action='store')
 
     args = parser.parse_args()
     sys.exit(main(args))
